95dc64e20ab1186f4f7d5abc7dc06888c1c4e633
EvyyNew <m15871626653@163.com>
2025-07-28 13:46:16 -0700

    UI

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ChatBot from '../components/Chatbot';
 
 export default function Home() {
+  const pageStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: '100vh',
+    padding: '20px',
+    backgroundColor: '#f0f2f5', // A soft background color for the page
+    boxSizing: 'border-box' // Ensures padding doesn't add to the total height
+  };
+
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', marginTop: 40 }}>
+    <div style={pageStyle}>
       <ChatBot />
     </div>
   );
