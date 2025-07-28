bb0950fd190543ad4b43a948b103fd0276444170
EvyyNew <152331105+EvyyNew@users.noreply.github.com>
2025-07-28 14:40:55 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ChatBot from '../components/Chatbot';
 
 export default function Home() {
+  const pageStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    paddingTop: '40px',
+  };
+
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', marginTop: 40 }}>
+    <div style={pageStyle}>
       <ChatBot />
     </div>
   );
